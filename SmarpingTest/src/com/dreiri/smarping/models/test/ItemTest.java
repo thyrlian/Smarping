fc2463a9b6ebad3400a6e85549759171fad9e433
Jing Li <thyrlian@gmail.com>
2013-11-26 01:40:21 +0100

    Added corresponding test for Item

---
+++ SmarpingTest/src/com/dreiri/smarping/models/test/ItemTest.java
@@ -0,0 +1,28 @@
+package com.dreiri.smarping.models.test;
+
+import android.test.AndroidTestCase;
+
+import com.dreiri.smarping.models.Item;
+
+public class ItemTest extends AndroidTestCase {
+    
+    Item itemA;
+    Item itemB;
+    Item itemC;
+    Item itemD;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        itemA = new Item("Milk");
+        itemB = new Item("milk");
+        itemC = new Item(" Milk ");
+        itemD = new Item("Bread");
+    }
+
+    public void testEqualsItem() {
+        assertTrue(itemA.equals(itemB));
+        assertTrue(itemA.equals(itemC));
+        assertFalse(itemA.equals(itemD));
+    }
+
+}
