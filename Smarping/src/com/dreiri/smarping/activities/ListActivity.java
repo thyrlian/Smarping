6b82c04c5e6f0d91ded7973eb848c0cc724f6b77
Jing Li <thyrlian@gmail.com>
2013-12-04 22:59:18 +0100

    Removed hardcoded test data, added NewItem function

--- Smarping/src/com/dreiri/smarping/activities/ListActivity.java
+++ Smarping/src/com/dreiri/smarping/activities/ListActivity.java
@@ -10,12 +10,13 @@ import android.widget.Toast;
 
 import com.dreiri.smarping.R;
 import com.dreiri.smarping.adapters.ItemAdapter;
-import com.dreiri.smarping.models.Item;
 import com.dreiri.smarping.models.List;
 
 public class ListActivity extends Activity {
     
     private ListView listView;
+    public List list = new List();
+    public ItemAdapter itemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,32 +24,13 @@ public class ListActivity extends Activity {
         setContentView(R.layout.activity_list);
         ActionBar actionBar = getActionBar();
         actionBar.setHomeButtonEnabled(true);
-        List list = new List();
-        list.add(new Item("鸡蛋"));
-        list.add(new Item("牛奶"));
-        list.add(new Item("面包"));
-        list.add(new Item("美女"));
-        list.add(new Item("手机"));
-        list.add(new Item("漫画"));
-        list.add(new Item("饮料"));
-        list.add(new Item("酒水"));
-        list.add(new Item("寿司"));
-        list.add(new Item("篮球"));
-        list.add(new Item("电脑"));
-        list.add(new Item("手机"));
-        list.add(new Item("家电"));
-        list.add(new Item("极客装备"));
-        list.add(new Item("天"));
-        list.add(new Item("地"));
-        list.add(new Item("人"));
-        list.add(new Item("和"));
         listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(new ItemAdapter(this, list));
+        itemAdapter = new ItemAdapter(this, list);
+        listView.setAdapter(itemAdapter);
     }
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_list, menu);
         return true;
     }
@@ -57,7 +39,6 @@ public class ListActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-//                listView.setSelectionAfterHeaderView();
                 listView.smoothScrollToPosition(0);
                 break;
             case R.id.action_add:
--- Smarping/src/com/dreiri/smarping/fragments/NewItemFragment.java
+++ Smarping/src/com/dreiri/smarping/fragments/NewItemFragment.java
@@ -1,20 +1,74 @@
+
 package com.dreiri.smarping.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnKeyListener;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.dreiri.smarping.R;
+import com.dreiri.smarping.activities.ListActivity;
+import com.dreiri.smarping.adapters.ItemAdapter;
+import com.dreiri.smarping.exceptions.AlreadyExists;
+import com.dreiri.smarping.models.Item;
+import com.dreiri.smarping.models.List;
 
 public class NewItemFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         FrameLayout frameLayout = (FrameLayout) inflater.inflate(R.layout.fragment_new_item, container, false);
+        EditText editTextNewItem = (EditText) frameLayout.findViewById(R.id.editTextNewItem);
+        editTextNewItem.setImeActionLabel("Add", KeyEvent.KEYCODE_ENTER);
+        editTextNewItem.setOnKeyListener(new OnKeyListener() {
+            @Override
+            public boolean onKey(final View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER || keyCode == KeyEvent.KEYCODE_DPAD_CENTER)) {
+                    EditText editTextNewItem = (EditText) v;
+                    ListActivity listActivity = (ListActivity) getActivity();
+                    List list = listActivity.list;
+                    ItemAdapter adapter = listActivity.itemAdapter;
+                    String itemName = editTextNewItem.getText().toString();
+                    getActivity().runOnUiThread(new ListUpdateTask(list, adapter, itemName));
+                    editTextNewItem.setText("");
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
         return frameLayout;
     }
 
+    private class ListUpdateTask implements Runnable {
+
+        private List list;
+        private BaseAdapter adapter;
+        private Item item;
+
+        public ListUpdateTask(List list, BaseAdapter adapter, String itemName) {
+            this.list = list;
+            this.adapter = adapter;
+            this.item = new Item(itemName);
+        }
+
+        @Override
+        public void run() {
+            try {
+                list.add(item);
+            } catch (AlreadyExists e) {
+                Toast.makeText(getActivity(), R.string.toast_already_exists, Toast.LENGTH_SHORT).show();
+            }
+            adapter.notifyDataSetChanged();
+        }
+
+    }
+    
 }
