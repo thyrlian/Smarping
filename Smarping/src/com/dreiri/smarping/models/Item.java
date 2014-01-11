1b5a1d0db2b13052d09169a0920e608ce6a8d26f
Jing Li <thyrlian@gmail.com>
2014-01-11 20:09:18 +0100

    Made Item parcelable.

--- Smarping/src/com/dreiri/smarping/models/Item.java
+++ Smarping/src/com/dreiri/smarping/models/Item.java
@@ -1,8 +1,11 @@
 package com.dreiri.smarping.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.dreiri.smarping.exceptions.NullValue;
 
-public class Item {
+public class Item implements Parcelable {
     
     public String name;
     
@@ -15,8 +18,35 @@ public class Item {
         }
     }
     
+    public Item(Parcel orig) {
+        new Item(orig.readString());
+    }
+    
     public boolean equals(Item item) {
         return name.equalsIgnoreCase(item.name);
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+    }
+    
+    public static final Parcelable.Creator<Item> CREATOR = new Parcelable.Creator<Item>() {
+
+        @Override
+        public Item createFromParcel(Parcel source) {
+            return new Item(source);
+        }
+
+        @Override
+        public Item[] newArray(int size) {
+            return new Item[size];
+        }
+    };
     
 }
