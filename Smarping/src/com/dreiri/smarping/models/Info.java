762f299c88da5d4cd89595c9101f3aa1caf47885
Jing Li <thyrlian@gmail.com>
2014-03-23 16:05:22 +0100

    Added more constructors for Info class.

--- Smarping/src/com/dreiri/smarping/models/Info.java
+++ Smarping/src/com/dreiri/smarping/models/Info.java
@@ -5,13 +5,30 @@ import java.util.Date;
 import com.google.android.gms.maps.model.LatLng;
 
 public class Info {
-    
+
     Date date;
     LatLng latLng;
-    
+
     public Info() {
-        // get current DateTime and Location(optional)
-        this.date = new Date();
+        this.date = getCurrentDate();
     }
-    
+
+    public Info(Date date) {
+        this.date = date;
+    }
+
+    public Info(LatLng latLng) {
+        this.date = getCurrentDate();
+        this.latLng = latLng;
+    }
+
+    public Info(Date date, LatLng latLng) {
+        this.date = date;
+        this.latLng = latLng;
+    }
+
+    private Date getCurrentDate() {
+        return new Date();
+    }
+
 }
